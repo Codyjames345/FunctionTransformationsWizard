adfbe1870f1dd63f48cd138d00fd0fc011362513
Codyjames345 <codyjameslincoln@gmail.com>
2023-02-16 21:44:58 +1100

    V2.0b Major Update

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Function Transformations Wizard (FTW™) V1.2.1
+# Function Transformations Wizard (FTW™) V2.0b
 ## created by Cody Lincoln for personal usage
 
 The program asks for two user inputs - the object and image functions - and returns a list of steps required to transform the object into the image.
 I started building this as a fun project but I would like to see it finished one day.
+Currently only x is supported as a variable and other unknowns will not be processed.
 
 Dependencies
 - RegEx Python library
@@ -13,20 +14,38 @@ Instructions
 3. get a Victory Royale in the popular third-person shooter game "Fortnite"
 4. run with any Python interpreter.
 
-TODO
+Planned Features
 - [x] allow spaces in user input
 - [x] support f(x) notation
-- [ ] allow variables other than x to be used
-- [ ] add support for other function formats like 1/x and sin(x)
+- [x] add support for other function formats like 1/x and sin(x)
+- [x] fix RegEx matches
+- [x] allow no brackets in cases like x**2
 - [ ] add check for different powers
-- [ ] more idiot-proofing
+- [ ] integrate simplification process
+- [ ] support fractions in both user input and function output when applicable
+
+
+Future Release
+- [ ] allow variables other than x to be used
+- [ ] allow input of unknown variables
+- [ ] allow user entry of object function and transformation steps and return image function (V3 Update)
+
+Scrapped Features
 - [ ] simplify steps including variables
-- [ ] fix RegEx matches
-- [ ] allow no brackets in cases like x**2
-- [ ] support fractions in both user input and function output whn applicable
-- [ ] allow user entry of object function and transformation steps and return image function (V2 Update)
 
 Changelog
+
+V2.0b - The Update That Changed The World (beta)
+- a variety of new functions are supported, including but not limited to
+    - sin(x), cos(x), tan(x)
+    - 1/x, 1/x**2 ...
+    - |x|
+- added FindGroup function which fixes cases where multiple RegEx matches occur
+- added NumConvert function which greatly de-spaghettifies the coefficient check
+- finally fixed the goofy ahh RegEx matches
+- greatly increased compatibility with alternate function layouts
+- moved some comments to be above the relevant code for readability
+- revised the scope of the project to be more relevant
 
 V1.2.1 - Snapshot Update
 - fixed variable reference in SanitiseInput function
--- transformationsWizard.py
+++ transformationsWizard.py
@@ -1,17 +1,29 @@
 # Function Transformations Wizard (FTW™) V2.0b
-# created by Cody Lincoln with the supervision of Stack Overflow
+# created by Cody Lincoln
+
+# this is a beta version, functionality might break with certain inputs (I haven't added input validation yet 💀)
+# some answers may be incorrect
 
 import re # import Python RegEx library
 
+def FindGroup(search): # define FindGroup function
+    for i in range(len(search.groups())): # iterate through length of groups tuple
+        if search.group(i+1) is not None: # find the first value that is not None
+            return search.group(i+1) # return the value
+    return None # every group contained None
+
 def GrabXY(func): # define GrabXY function
 
-    # grab x terms with RegEx or operators for multiple function formats
+    # grab x terms with RegEx
     x_match = re.compile(r"[(|]([^)|]*x+[^)|]*)[|)]|(x)") # this actually grabs the x terms correctly 99% of the time lol
 
-    # grab y terms with RegEx or operators for multiple function formats
-    y_match = re.compile(r"([a-zA-Z]*[(|][^)|]*x+[^)|]*[|)](?:\*\*[0-9.]+)*)|(x)")
+    # grab y terms with RegEx
+    # TODO more robust matching
+    y_match = re.compile(r"([a-zA-Z]*[(|][^)|]*x+[^)|]*[|)](?:\*\*[0-9.]+)*)|(x(?:\*\*[0-9.]+)*)")
 
-    x_part = re.search(x_match, func).group(1) # get all x (horizontal) transformations
+    # FindGroup function returns content of first group that is not None
+    # this means matching has left to right priority
+    x_part = FindGroup(re.search(x_match, func)) # get all x (horizontal) transformations
     y_part = re.sub(y_match, "f", func) # replace x portion with "f" which leaves y (vertical) transformations
 
     base_func = None # TODO grab base function
@@ -25,134 +37,113 @@ def SanitiseInput(rawInput): # define SanitiseInput funciton
     # strip whitespace from inputs
     cleanInput = re.sub(r'\s+', '', rawInput)
 
-    if "=" in rawInput:
+    if "=" in cleanInput:
         # get equation after equals sign to prevent f(x) notation triggering the RegEx
         # update function name with content before equals sign
         funcName, cleanInput = cleanInput.split("=")
 
-    #TODO more sanitisation
+    # TODO more sanitisation
 
     return [funcName, cleanInput] # return function name and sanitised input in a list
+def NumConvert(number, default): # define NumConvert function
+    try:
+        if number is None or number == "" or number == "-": # if user inputted "-" or omitted coefficient
+            # return fixed number with default value appended (- becomes -1, no coefficient becomes 1)
+            # default for translation is 0, dilation is 1
+            return float(f"{number}{default}")
+        else:
+            return float(number) # else return input
+    except ValueError: # if input was bad
+        return "ValueError - Bad input" # return error
+
 
 def FindSeq(obj_raw, img_raw): # define FindSeq function
 
     # call SanitiseInput function on both the object and image
-    obj_san, img_san = SanitiseInput(obj_raw), SanitiseInput(img_raw)
-    
     # store function name and equation (after equals sign)
-    obj_func, obj = obj_san
-    img_func, img = img_san
+    obj_func_name, obj_sanitised = SanitiseInput(obj_raw)
+    img_func_name, img_sanitised = SanitiseInput(img_raw)
 
     # grab separated x and y parts from GrabXY function
-    obj_base_func, obj_x_part, obj_y_part = GrabXY(obj)
-    img_base_func, img_x_part, img_y_part = GrabXY(img)
+    obj_base_func, obj_x_part, obj_y_part = GrabXY(obj_sanitised)
+    img_base_func, img_x_part, img_y_part = GrabXY(img_sanitised)
 
-    # get variables from object and image
-    # TODO get rid of spaghetti RegEx
-    # TODO use GrabXY function
-    a = re.search(r"(\-*[a-zA-Z0-9.]+)\(", obj)
-    ad = re.search(r"(\-*[a-zA-Z0-9.]+)\(", img)
-    b = re.search(r"(\-*[a-zA-Z0-9.]+)x", obj)
-    bd = re.search(r"(\-*[a-zA-Z0-9.]+)x", img)
-    h = re.search(r"x([-+][a-zA-Z0-9.]+)", obj)
-    hd = re.search(r"x([-+][a-zA-Z0-9.]+)", img)
-    k = re.search(r"([-+][a-zA-Z0-9.]+)$", obj)
-    kd = re.search(r"([-+][a-zA-Z0-9.]+)$", img)
+    # strip "/" in 1/x cases so that b/f = bf where f = (1/x) which is a dilation of b from the x-axis (vertical)
+    obj_y_part = obj_y_part.replace("/", "")
+    img_y_part = img_y_part.replace("/", "")
 
-    # set default values for shorthand writing (e.g. x = 1x) if match not found
-    # capture variables with regex group
+    # get transformation variables from object and image using RegEx groups
+    # TODO parse maths with library rather than hardcode
 
-    # check for negative sign without number
-    if a is None:
-        a = re.search(r"(\-\()", obj)
-        if a is None: a = 1
-        else: a = -1
-    else: a = a.group(1)
-    if ad is None:
-        ad = re.search(r"(\-\()", img)
-        if ad is None: ad = 1
-        else: ad = -1
-    else: ad = ad.group(1)
-    if b is None:
-        b = re.search(r"(\-x)", obj)
-        if b is None: b = 1
-        else: b = -1
-    else: b = b.group(1)
-    if bd is None:
-        bd = re.search(r"(\-x)", img)
-        if bd is None: bd = 1
-        else: bd = -1
-    else: bd = bd.group(1)
+    # call NumConvert function on each variable to properly format as floating point number
 
-    # test for alternate layout h - bx
-    if h is None:
-        h = re.search(r"\((\-*[a-zA-Z0-9.]+)[-+]", obj)
-        if h is None: h = 0
-        else: h = h.group(1)
-    else: h = h.group(1)
-    if hd is None:
-        hd = re.search(r"\((\-*[a-zA-Z0-9.]+)[-+]", img)
-        if hd is None: hd = 0
-        else: hd = hd.group(1)
-    else: hd = hd.group(1)
+    # finds coefficient of x (dilate from y-axis horizontal)
+    dil_x_match = re.compile(r"([+-]?[0-9.]*)x")
+    b = NumConvert(FindGroup(re.search(dil_x_match, obj_x_part)), 1)
+    bd = NumConvert(FindGroup(re.search(dil_x_match, img_x_part)), 1)
 
-    if k is None: k = 0
-    else: k = k.group(1)
-    if kd is None: kd = 0
-    else: kd = kd.group(1)
+    # finds coefficient of f (dilate from x-axis vertical)
+    dil_y_match = re.compile(r"([+-]?[0-9.]*)f")
+    a = NumConvert(FindGroup(re.search(dil_y_match, obj_y_part)), 1)
+    ad = NumConvert(FindGroup(re.search(dil_y_match, img_y_part)), 1)
+
+    # finds added or subtracted float in x part (horizontal translation)
+    trans_x_match = re.compile(r"([+-]?[0-9.]*)(?![0-9.]*x)")
+    h = NumConvert(FindGroup(re.search(trans_x_match, obj_x_part)), 0)
+    hd = NumConvert(FindGroup(re.search(trans_x_match, img_x_part)), 0)
+
+    # finds added or subtracted float in y part (vertical translation)
+    trans_y_match = re.compile(r"([+-]?[0-9.]*)(?![0-9.]*f)")
+    k = NumConvert(FindGroup(re.search(trans_y_match, obj_y_part)), 0)
+    kd = NumConvert(FindGroup(re.search(trans_y_match, img_y_part)), 0)
 
     steps = [] # initiate empty steps list
 
-    # dilation/reflection from x-axis
-    try:
-        dil_x = float(ad)/float(a)
-        if dil_x < 0:
-            steps.append("Reflect across the x-axis") # reflect if negative dilation
-            dil_x = -dil_x
-        if dil_x != 1.0:
-            steps.append(f"Dilate * {dil_x} units from the x-axis") # dilation from x-axis = b/bd
-    except ValueError:
-        steps.append(f"Dilate * {ad}/{a} units from the x-axis") # variable was entered
-    
-    # dilation/reflection from y-axis
-    try:
-        dil_y = float(b)/float(bd)
-        if dil_y < 0:
-            steps.append("Reflect across the y-axis") # reflect if negative dilation
-            dil_y = -dil_y
-        if dil_y != 1.0:
-            steps.append(f"Dilate * {dil_y} units from the y-axis") # dilation from y-axis = ad/a
-    except ValueError:
-        steps.append(f"Dilate * {b}/{bd} units from the y-axis") # variable was entered
+    # dilation/reflection from y-axis (horizontal)
+    dil_x = b/bd
+    if dil_x < 0:
+        steps.append("Reflect across the y-axis") # reflect if negative dilation
+        dil_x = -dil_x
+    if dil_x != 1.0:
+        steps.append(f"Dilate * {dil_x} units from the y-axis") # dilation from y-axis = b/bd, if dil_x = 1 nothing will be added
+
+    # dilation/reflection from x-axis (vertical)
+    dil_y = ad/a
+    if dil_y < 0:
+        steps.append("Reflect across the x-axis") # reflect if negative dilation
+        dil_y = -dil_y
+    if dil_y != 1.0:
+        steps.append(f"Dilate * {dil_y} units from the x-axis") # dilation from x-axis = ad/a, if dil_y = 1 nothing will be added
 
     # translation in x-axis
-    try: 
-        trans_x = -(float(hd)-float(h))/float(bd) # negative because -h = translation of +h right
-        if trans_x < 0:
-            steps.append(f"Translate {-trans_x} units to the left") # negative translation right = positive translation left
-        elif trans_x > 0:
-            steps.append(f"Translate {trans_x} units to the right") # translate (hd-h)/bd right, if trans_x = 0 nothing will be added
-    except ValueError:
-        steps.append(f"Translate ({hd}-{h})/{bd} right") # variable was entered
+    trans_x = -(hd-h)/bd # negative because -h = translation of +h right
+    if trans_x < 0:
+        steps.append(f"Translate {-trans_x} units to the left") # negative translation right = positive translation left
+    elif trans_x > 0:
+        steps.append(f"Translate {trans_x} units to the right") # translate (hd-h)/bd right, if trans_x = 0 nothing will be added
     
     # translation in y-axis
-    try: 
-        trans_y = (float(kd)*float(a)-float(k)*float(ad))/float(a)
-        if trans_y < 0:
-            steps.append(f"Translate {-trans_y} units down") # negative translation up = positive translation down
-        elif trans_y > 0:
-            steps.append(f"Translate {trans_y} units up") # translate (kd*a-k*ad)/a up, if trans_y = 0 nothing will be added
-    except ValueError:
-        steps.append(f"Translate ({kd}*{a}-{k}*{ad})/{a} up") # variable was entered
+    trans_y = (kd*a-k*ad)/a
+    if trans_y < 0:
+        steps.append(f"Translate {-trans_y} units down") # negative translation up = positive translation down
+    elif trans_y > 0:
+        steps.append(f"Translate {trans_y} units up") # translate (kd*a-k*ad)/a up, if trans_y = 0 nothing will be added
 
-    return [obj_func, img_func, steps] # return function names and list of calculated steps
+    return [obj_func_name, img_func_name, steps] # return function names and list of calculated steps
+
 
 # not needed for now as multiple function types are supported (beta)
 # print("Enter all equations in the form y = a(bx-h)**n+k")
-obj = input("Enter the object: ")
-img = input("Enter the image: ") # input object and image functions from user
 
-obj_func, img_func, steps = FindSeq(obj, img) # get function names and list of steps from FindSeq function
+# testing
+# print(float("-0"))
+
+# input object and image functions from user
+obj = input("Enter the object: ")
+img = input("Enter the image: ")
+
+# get function names and list of steps from FindSeq function
+obj_func, img_func, steps = FindSeq(obj, img)
 
 if obj_func and img_func:
     print(f"Steps required to transform {obj_func} --> {img_func}")
